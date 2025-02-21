8f24a97785202317bdc5e338a7f2801c1600d91e
Blake Arnold <barnold@blakearnold.me>
2025-02-21 17:03:59 -0500

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-USER=barnold
+# Check if username parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Username is required"
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+USER=$1  # First parameter becomes the username
 HOST=blakearnold.me
 DIR=/var/www/blakearnold.me/public_html  # the directory where your website files should go
 
